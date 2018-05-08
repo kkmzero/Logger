b3986af18af2ba09f4411116295281d3a3950669
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2018-05-08 20:00:47 +0200

    Create Logger.h

---
+++ Logger.h
@@ -0,0 +1,25 @@
+/*
+ * Logger
+ * Copyright (c) 2018 Ivan Kmeťo
+ *
+ * Licensed under the MIT License.
+ * http://opensource.org/licenses/mit-license
+*/
+
+#pragma once
+
+#include <iostream>
+
+void Logger(std::string func, std::string file, int line);
+
+#define Error(x)\
+	if (x) {\
+	Logger(#x, __FILE__, __LINE__);\
+}
+
+bool Log_Open(std::string);
+bool Log_Close();
+void Log_Write(std::string);
+void Log_Init(std::string);
+void Log_End(std::string);
+void Log_WriteIn(std::string text, std::string);
